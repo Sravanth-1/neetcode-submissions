a5de77e416bbb40fd54e5940b1256c8e52de9af5
SAI SRAVANTH DATTI <110086863+Sravanth-1@users.noreply.github.com>
2026-05-18 09:58:28 +0900

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> freq(26, 0);
+        int maxfreq = 0;
+        int last = 0;
+        int maxlen = 0;
+        for(int i = 0; i < s.size(); i++){
+            freq[s[i]-'A']++;
+            maxfreq = max(maxfreq, freq[s[i]-'A']);
+            if(last < s.size() && i-last+1-maxfreq > k){
+                freq[s[last]-'A']--;
+                last++;
+            }
+            maxlen = max(i-last+1, maxlen);
+        }
+        return maxlen;
+    }
+};
